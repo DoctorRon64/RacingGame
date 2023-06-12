7b044c997497202d626ee682f50292331af48bbd
DrRon <piepmail@lefthandmedia.com>
2023-06-12 16:01:15 +0200

    More Cars Spawn Now

--- RACER/GameManager.cpp
+++ RACER/GameManager.cpp
@@ -9,64 +9,60 @@ GameManager::GameManager(sf::RenderWindow* win, TextureLibrary* TLib, float sW, 
 {
     float randomValue = randomFloat(0, screenWidth);
 
-    CarAmount = 10;
+    CarAmount = 3;
 
     SetText();
 
     player = new Player(500, (sH - 100), 0.12f, 0.12f, 2000, 4.0f, 1, textureLibrary->PlayerTexture, window);
-    carObj = new Car(0, -100, .1f, .1f, randomFloat(400, 600), 30, 10, textureLibrary->CarTexture, window);
-    colletect = new CollisionDetect(5, 5);
 
+    Car* carObj = new Car(-1000, -1000, .1f, .1f, 0, 1, 0, textureLibrary->CarTexture, window);
     carWidth = carObj->getCarWidth();
+    delete carObj;
+
+    colletect = new CollisionDetect(5, 5);
+    carList = std::vector<Car*>();
+
+    CreateCars();
 }
 
 GameManager::~GameManager()
 {
-    delete carObj;
     delete player;
     delete textureLibrary;
     delete colletect;
+
+    for (int i = 0; i < carList.size(); i++)
+    {
+        carList.erase(carList.begin() + i);
+        delete carList[i];
+    }
 }
 
 void GameManager::Update(float deltaTime)
 {
     player->update(screenWidth, deltaTime);
-    carObj->update(deltaTime);
 
-    if (colletect->ReturnDetectValue(player->getPosition(), carObj->getPosition()) <= 0)
+    for (int i = 0; i < carList.size(); i++)
     {
-        delete carObj;
-        carObj = nullptr;
-        Score++;
-        ScoreToEnd++;
-        CreateCar();
+        carList[i]->update(deltaTime);
+
+        if (colletect->ReturnDetectValue(player->getPosition(), carList[i]->getPosition()) <= 0)
+        {
+            Score++;
+            ScoreToEnd++;
+            carList.erase(carList.begin() + i);
+            delete carList[i];
+        }
+
+        if (carList[i]->CheckIfDeath(screenHeight) == true)
+        {
+            ScoreToEnd++;
+            carList.erase(carList.begin() + i);
+            delete carList[i];
+            CreateCars();
+        }
     }
 
-    if (carObj->CheckIfDeath(screenHeight) == true)
-    {
-        delete carObj;
-        carObj = nullptr;
-        ScoreToEnd++;
-        CreateCar();
-    }
-
-    scoreDisplay.setString(std::to_string(Score));
-    std::cout << scoreDisplay.getString().toAnsiString() << std::endl;
-
-    window->draw(scoreDisplay);
-    window->draw(TextDisplay);
-}
-
-float GameManager::randomFloat(float min, float max)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(min, max);
-    return dis(gen);
-}
-
-void GameManager::CreateCar()
-{
     if (ScoreToEnd >= 10)
     {
         if (Score >= ScoreToEnd / 2)
@@ -79,9 +75,31 @@ void GameManager::CreateCar()
         }
     }
 
-    float maxSize = randomFloat(0, screenWidth - carWidth);
-    carObj = new Car(maxSize, -100, .1f, .1f, randomFloat(400, 600), 30, 10, textureLibrary->CarTexture, window);
+    scoreDisplay.setString(std::to_string(Score));
+    std::cout << scoreDisplay.getString().toAnsiString() << std::endl;
+
+    window->draw(scoreDisplay);
+    window->draw(TextDisplay);
 }
+
+void GameManager::CreateCars()
+{
+    for (int i = 0; i < CarAmount; i++)
+    {
+        float maxSize = randomFloat(0, screenWidth - carWidth);
+        carList.push_back(new Car(maxSize, -100, .1f, .1f, randomFloat(400, 600), 30, 10, textureLibrary->CarTexture, window));
+    }
+}
+
+float GameManager::randomFloat(float min, float max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min, max);
+    return dis(gen);
+}
+
+
 
 void GameManager::SetText()
 {
--- RACER/GameManager.h
+++ RACER/GameManager.h
@@ -11,24 +11,24 @@ class GameManager
 {
 private:
     Player* player;
-    Car* carObj;
     TextureLibrary* textureLibrary;
     sf::RenderWindow* window;
     CollisionDetect* colletect;
     sf::Text scoreDisplay;
     sf::Text TextDisplay;
+    std::vector<Car*> carList;
     float screenWidth;
     float screenHeight;
     float carWidth;
 
     void SetText();
+    void CreateCars();
 
 public:
     GameManager(sf::RenderWindow* win, TextureLibrary* TLib, float sW, float sH);
     ~GameManager();
     void Update(float deltaTime);
     float randomFloat(float min, float max);
-    void CreateCar();
 
     int Score;
     int ScoreToEnd;
